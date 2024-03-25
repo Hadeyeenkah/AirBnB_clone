7766a46a1587bee76fe2416a4eb2949251d520f5
hadeyeenkah <tboysammy@gmail.com>
2024-03-24 21:51:01 -0700

    Airbnb

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,17 @@
+#!/usr/bin/phython3
+"""Defines unittests for base.py
+"""
+import unittests
+from models.base_model import BaseModel
+
+class Test_attribute(unittest.TestCase):
+    def base1(self):
+        b1= Basemodel()
+        self.assertEqual(b1.id, "")
+
+        def base2(self )
+        self.assertEqual(Base().id,"")
+
+        def base2(self)
+        self.assertEqual(
+
