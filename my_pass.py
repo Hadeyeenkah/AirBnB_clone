716587083004efed33a4d547e521dfc2b6242793
hadeyeenkah <tboysammy@gmail.com>
2024-03-26 02:23:38 -0700

    my task1

--- my_pass.py
+++ my_pass.py
@@ -7,14 +7,14 @@ class phone:
         self.color = color
 
         def Display_info(self):
-        """Display the infomation about the phone."""
-        info = f"phone: {self.color} {self.make} {self.model}
-        print(info)
+            """Display the infomation about the phone."""
+            info = f"phone: {self.make}, Color: {self.color} {self.model}
+            print(info)
 
-        def main():
-        """main function."""
-        my_phone = Phone("Iphone", "Iphone15promax", "Red")
-        my_phone.display_info()
+            def main():
+            """main function."""
+            my_phone = Phone("Iphone", "Iphone15promax", "Red")
+            my_phone.display_info()
 
-        if __name__ == "__main__":
-        main()
+            if __name__ == "__main__":
+            main()
