6b7faa4111658de8a51d8d7e48df5bf20466d7cf
solotole <solomontoleak47@gmail.com>
2024-04-24 10:52:11 -0700

    console.py

--- console.py
+++ console.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """Entry point of the command interpreter"""
 import cmd
-from models.base_model import BaseModel
 from models import storage
-
+from models.base_model import BaseModel
 
 class HBNBCommand(cmd.Cmd):
     """Class definition of the entry point of thr command interpreter"""
@@ -44,8 +43,6 @@ class HBNBCommand(cmd.Cmd):
         """command that Prints the string representation of an
             instance based on the class name and id
         """
-        # from models.engine.file_storage import FileStorage
-        # storage = FileStorage()
         lists = args.split()
         if not args:
             print("** class name missing **")
@@ -61,18 +58,15 @@ class HBNBCommand(cmd.Cmd):
 
         key = f"{lists[0]}.{lists[1]}"
         dictionary = storage.all()
-        obj = dictionary.get(key)
-        if not obj:
+        if key not in dictionary:
             print("** no instance found **")
             return
-
+        obj = dictionary.get(key)
         print(obj)
 
     def do_destroy(self, line):
         """Deletes an instance based on the class name and id
         """
-        # from models.engine.file_storage import FileStorage
-        # storage = FileStorage()
         lists = line.split()
         if not lists:
             print("** class name missing **")
@@ -83,10 +77,10 @@ class HBNBCommand(cmd.Cmd):
         if len(lists) < 2:
             print("** instance id missing **")
             return
-        key = f"{lists[1]}.{lists[1]}"
+        key = f"{lists[0]}.{lists[1]}"
         dictionary = storage.all()
-        obj = dictionary.get(key)
-        if not obj:
+        # obj = dictionary.get(key)
+        if key not in dictionary:
             print("** no instance found **")
             return
         del dictionary[key]
@@ -96,8 +90,6 @@ class HBNBCommand(cmd.Cmd):
         """Prints all string representation of all
             instances based or not on the class name
         """
-        # from models.engine.file_storage import FileStorage
-        # storage = FileStorage()
         listing = []
         lists = line.split()
         if lists[0] not in ["BaseModel"]:
@@ -106,51 +98,54 @@ class HBNBCommand(cmd.Cmd):
 
         if not lists:
             dictionary = storage.all()
-            print(list(dictionary))
+            for key in dictionary.values():
+                listing.append(str(key.__dict__))
+            print(listing)
             return
 
-        dictionary = storage.all()
-        for key in dictionary.key():
-            if key.startswith(lists[0]):
-                listing.append(dictionary[key])
-        print("listing")
+        if lists:
+            dictionary = storage.all()
+            for value in dictionary.values():
+                listing.append(value.__str__())
+            print(listing)
+            return
 
     def do_update(self, line):
         """Updates an instance based on the class name and id
             by adding or updating attribute
         """
-        # from models.engine.file_storage import FileStorage
-        # storage = FileStorage()
         lists = line.split()
-        if len(list) > 4:
-            lists1 = lists[0:3]
-        if not lists1:
+        lists1 = []
+        if not lists:
             print("** class name missing **")
             return
 
-        if lists1[0] not in ["BaseModel"]:
+        if lists[0] not in ["BaseModel"]:
             print("** class doesn't exist **")
             return
 
-        if len(lists1) < 2:
+        if len(lists) < 2:
             print("** instance id missing **")
             return
 
-        key = f"{lists1[0]}.{lists1[1]}"
+        key = f"{lists[0]}.{lists[1]}"
         dictionary = storage.all()
         if key not in dictionary:
             print("** no instance found **")
             return
-        if len(lists1) < 3:
-            print("** attribute name missing **")
-            return
-        if len(lists1) == 4:
-            dictionary1 = dictionary[key]
-            try:
-                dictionary1[lists1[2]] = lists1[3]
-            except KeyError:
-                dictionary1[lists1[2]] = lists1[3]
-        storage.save()
+        for key_keys in dictionary.keys():
+            if key_keys == key:
+                if len(lists) < 3:
+                    print("** attribute name missing **")
+                    return
+                if len(lists) < 4:
+                    print("** value missing **")
+                    return
+                if len(lists) > 4 or len(lists) == 4:
+                    lists1 = lists[0:4]
+                    dict_value = dictionary[key]
+                    dict_value.__dict__[lists[2]] = lists[3]
+                    storage.save()
 
 
 if __name__ == '__main__':
